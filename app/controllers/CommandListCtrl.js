a5fc83777c16d956c1440981288cfbf7722a6406
Talbot <talbotlawrence75@gmail.com>
2017-03-17 15:54:32 -0500

    my delete function is working

--- app/controllers/CommandListCtrl.js
+++ app/controllers/CommandListCtrl.js
@@ -8,17 +8,18 @@ app.controller("CommandListCtrl", function($scope, CommandStorage, SearchTermDat
 	CommandStorage.getCommandList(user)
 		.then(function(commandCollection){
 			console.log("user", user);
-			console.log("commandCollection", commandCollection);
+			console.log("commandCollection from CommandListCtrl.js", commandCollection);
 			$scope.commands = commandCollection;
 		});
 
-	$scope.commandDelete = function(commandId){
-		CommandStorage.deleteCommand(commandId)
-			.then(function(response) {
-				CommandStorage.getCommandList(user).then(function(commandCollection){
-					console.log("commandCollection", commandCollection);
-					$scope.commands = commandCollection;
-				});
-			});
-	};
+	// $scope.commandDelete = function(commandId){
+	// 	console.log("commandId from commandDelete", commandId);
+	// 	CommandStorage.deleteCommand(commandId)
+	// 		.then(function(response) {
+	// 			CommandStorage.getCommandList(user).then(function(commandCollection){
+	// 				console.log("commandCollection from the delete", commandCollection);
+	// 				$scope.commands = commandCollection;
+	// 			});
+	// 		});
+	// };
 });
--- app/controllers/CommandViewCtrl.js
+++ app/controllers/CommandViewCtrl.js
@@ -7,10 +7,23 @@ app.controller("CommandViewCtrl", function($scope, $routeParams, CommandStorage,
 
 	CommandStorage.getCommandList(user)
 		.then(function(commandCollection) {
+			console.log("commandCollection from CommandViewCtrl.js", commandCollection);
 			$scope.commands = commandCollection;
 
 			$scope.selectedCommand = $scope.commands.filter(function(command){
+				console.log("command potato from CommandViewCtrl", command);
 				return command.id === $routeParams.commandId;
 			})[0];
 		});
+
+		$scope.commandDelete = function(commandId){
+		console.log("commandId from commandDelete", commandId);
+		CommandStorage.deleteCommand(commandId)
+			.then(function(response) {
+				CommandStorage.getCommandList(user).then(function(commandCollection){
+					console.log("commandCollection from the delete", commandCollection);
+					$scope.commands = commandCollection;
+				});
+			});
+	};
 });
