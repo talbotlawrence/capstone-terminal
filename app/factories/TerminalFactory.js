da2c187d9690171154485ea211f513b1eebd1fe5
Talbot <talbotlawrence75@gmail.com>
2017-06-07 18:51:08 -0500

    trying to read the json file

--- app/factories/TerminalFactory.js
+++ app/factories/TerminalFactory.js
@@ -1,87 +1,111 @@
 'use strict';
 
-app.factory("CommandStorage", ($q, $http) => {	///////////DON'T NEED FBCreds???
+app.factory("CommandStorage", ($q, $http) => {  ///////////DON'T NEED FBCreds???
 
-	let getCommandList = () => {	///////////DO YOU NEED (user)???
-		let commands = [];
-		return $q((resolve, reject) => {
-			// $http.get(`${FBCreds.databaseURL}/commands.json?orderBy="uid"&equalTo="${user}"`)
-			// $http.get(`${FBCreds.databaseURL}/commands.json`)	///////////DO YOU NEED???
-			// $http.get(`../../commands.json`)
-			// $.getJSON('../../commands.json')
-			$.ajax({
-				url: '../../commands.json',
-				dataType: 'json',
-				type: 'get',
-				cache: false,
-				success: function(data) {
-					console.log(data);
-				}
-			})
+    let getCommandList = () => {    ///////////DO YOU NEED (user)???
+        let commands = [];
+        // console.log("commands array: ", commands);
+        return $q((resolve, reject) => {
+            // console.log("resolve: ", resolve);
+            $http.get('../../commands.json')
+            .then((commandObject) => {
+                // console.log("commandObject: ", commandObject);
+                let commandCollection = commandObject.data;
+                console.log("commandCollection commands: ", commandCollection.commands);
+                // console.log("Object keys", Object.keys(commandCollection.commands));
+                Object.keys(commandCollection.commands).forEach((key) => {   //changed--add .commands
+                    // console.log("key: ", key);
+                    // commandCollection[key].id = key;
+                    commandCollection[key] = key;        //added whole line
+                    // console.log("commandCollection[key]: ", commandCollection[key]);
 
-				.then((commandObject) => {
-					let commandCollection = commandObject.data;
-					// console.log("commandCollection", commandCollection);
-					Object.keys(commandCollection).forEach((key) => {
-						commandCollection[key].id = key;
-						commands.push(commandCollection[key]);
-					});
-					// commands = commands.sort();
-					resolve(commands);
-				})
-			.catch((error) => {
-				reject(error);
-			});
-		});
-	};
+                    commands.push(commandCollection[key]);
+                    // console.log("commands: ", commands);
+                });
+                resolve(commands);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+        });
+    };
 
-	// let postNewCommand = (newCommand) =>  {
-	// 	return $q((resolve, reject) => {
-	// 		$http.post(`${FBCreds.databaseURL}/commands.json`,
-	// 				JSON.stringify(newCommand))
-	// 			.then((ObjectFromFirebase) => {
-	// 				resolve(ObjectFromFirebase);
-	// 			})
-	// 		.catch((error) => {
-	// 			reject(error);
-	// 		});
-	// 	});
-	// };
 
-	// let deleteCommand = (commandId) => {
-	// 	console.log("delete in factory", commandId);
-	// 	return $q((resolve, reject) => {
-	// 		$http.delete(`${FBCreds.databaseURL}/commands/${commandId}.json`)
-	// 			.then((ObjectFromFirebase) => {
-	// 				resolve(ObjectFromFirebase);
-	// 			});
-	// 	});
-	// };
 
-	let getSingleCommand = (commandId) => {
-		return $q(function(resolve, reject) {
-			// $http.get(`${FBCreds.databaseURL}/commands/${commandId}.json`)	///////////DO YOU NEED FBCreds???
+    // $.ajax({
+    //  url: '../../commands.json',
+    //  dataType: 'json',
+    //  type: 'get',
+    //  cache: false,
+    //  success: function(data) {
+    //      console.log(data);
+    //  }
+    // })
 
-				.then(function(commandObject) {
-					resolve(commandObject.data);
-				})
-			.catch(function(error) {
-				reject(error);
-			});
-		});
-	};
+    // $.get( "../../commands.json", function( data ) {
+    //          $( ".result" ).html( data );
+    //          alert( "Load was performed." );
+    //      })
 
-	// let updateCommand = (commandId, editedCommand) => {
-	// 	return $q(function(resolve, reject) {
-	// 		$http.patch(`${FBCreds.databaseURL}/commands/${commandId}.json`, angular.toJson(editedCommand))
-	// 			.then(function(ObjectFromFirebase) {
-	// 				resolve(ObjectFromFirebase);
-	// 			})
-	// 		.catch(function(error) {
-	// 			reject(error);
-	// 		});
-	// 	});
-	// };
+    // $http.get(`${FBCreds.databaseURL}/commands.json?orderBy="uid"&equalTo="${user}"`)
+            // $http.get(`${FBCreds.databaseURL}/commands.json`)    ///////////DO YOU NEED???
+            // $http.get(`../../commands.json`)
 
-	return {getCommandList, getSingleCommand};	///////////DO YOU NEED ALL OF THESE???
+                    // $.getJSON('../../commands.json')
+
+
+
+
+
+
+    // let postNewCommand = (newCommand) =>  {
+    //  return $q((resolve, reject) => {
+    //      $http.post(`${FBCreds.databaseURL}/commands.json`,
+    //              JSON.stringify(newCommand))
+    //          .then((ObjectFromFirebase) => {
+    //              resolve(ObjectFromFirebase);
+    //          })
+    //      .catch((error) => {
+    //          reject(error);
+    //      });
+    //  });
+    // };
+
+    // let deleteCommand = (commandId) => {
+    //  console.log("delete in factory", commandId);
+    //  return $q((resolve, reject) => {
+    //      $http.delete(`${FBCreds.databaseURL}/commands/${commandId}.json`)
+    //          .then((ObjectFromFirebase) => {
+    //              resolve(ObjectFromFirebase);
+    //          });
+    //  });
+    // };
+    ////////////////////////////////////////
+    // let getSingleCommand = (commandId) => {
+    //     return $q(function(resolve, reject) {
+    //         // $http.get(`${FBCreds.databaseURL}/commands/${commandId}.json`)   ///////////DO YOU NEED FBCreds???
+
+    //             .then(function(commandObject) {
+    //                 resolve(commandObject.data);
+    //             })
+    //         .catch(function(error) {
+    //             reject(error);
+    //         });
+    //     });
+    // };
+    ////////////////////////////////////////
+    // let updateCommand = (commandId, editedCommand) => {
+    //  return $q(function(resolve, reject) {
+    //      $http.patch(`${FBCreds.databaseURL}/commands/${commandId}.json`, angular.toJson(editedCommand))
+    //          .then(function(ObjectFromFirebase) {
+    //              resolve(ObjectFromFirebase);
+    //          })
+    //      .catch(function(error) {
+    //          reject(error);
+    //      });
+    //  });
+    // };
+
+    // return {getCommandList, getSingleCommand};   ///////////DO YOU NEED ALL OF THESE???
+    return {getCommandList};
 });
