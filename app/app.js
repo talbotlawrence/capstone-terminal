0cf63ba87cb20a6dadc433e790778cc37dcc96b1
Talbot <talbotlawrence75@gmail.com>
2017-06-08 14:21:30 -0500

    got the GUI displaying info again

--- app/app.js
+++ app/app.js
@@ -2,59 +2,17 @@
 
 var app = angular.module("TerminalApp", ["ngRoute"]);
 
-let isAuth = (AuthFactory) => new Promise ( (resolve, reject) => {
-	// console.log("running isAuth");
-	AuthFactory.isAuthenticated()
-		.then ( (userExists) => {
-			console.log("userExists", userExists);
-			if (userExists){
-				console.log("Authenticated--nice job typing");
-				resolve();
-			}else {
-				console.log("Authentication rejected--you suck!");
-				reject();
-			}
-		});
-});
-
 app.config(function($routeProvider){
 	$routeProvider.
 		when('/', {
-			templateUrl: 'partials/login.html',
-			controller:"UserCtrl"
-				// resolve: {isAuth}
+			templateUrl: 'partials/command-list.html',
+			controller:"CommandListCtrl"
 		}).
-	when('/login', {
-		templateUrl: 'partials/login.html',
-		controller:"UserCtrl"
-			// resolve: {isAuth}
+		when('/commands/:commandId', {
+			templateUrl: "partials/command-details.html",
+			controller: "CommandViewCtrl",
 	}).
-	when('/logout', {
-		templateUrl: 'partials/login.html',
-		controller:"UserCtrl"
-			// resolve: {isAuth}
-	}).
-	when('/commands/list',{
-		templateUrl: "partials/command-list.html",
-		controller: 'CommandListCtrl',
-		resolve: {isAuth}
-	}).
-	when('/commands/new', {
-		templateUrl: "partials/command-form.html",
-		controller: 'CommandNewCtrl',
-		resolve: {isAuth}
-	}).
-	when('/commands/:commandId', {
-		templateUrl: "partials/command-details.html",
-		controller: "CommandViewCtrl",
-		resolve: {isAuth}
-	}).
-	when('/commands/:commandId/edit', {
-		templateUrl: "partials/command-edit.html",
-		controller: "CommandEditCtrl",
-		resolve: {isAuth}
-	}).
-	otherwise('/commands/list');
+	otherwise('/');
 });
 
 app.run(($location, FBCreds) => {
