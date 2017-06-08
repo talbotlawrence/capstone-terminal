9a1f85c413ec3245e045868c2583ea613497e63d
Talbot <talbotlawrence75@gmail.com>
2017-06-08 15:16:52 -0500

    still cleaning

--- app/factories/TerminalFactory.js
+++ app/factories/TerminalFactory.js
@@ -23,28 +23,28 @@ app.factory("CommandStorage", ($q, $http, FBCreds) => {
 		});
 	};
 
-	let postNewCommand = (newCommand) =>  {
-		return $q((resolve, reject) => {
-			$http.post(`${FBCreds.databaseURL}/commands.json`,
-					JSON.stringify(newCommand))
-				.then((ObjectFromFirebase) => {
-					resolve(ObjectFromFirebase);
-				})
-			.catch((error) => {
-				reject(error);
-			});
-		});
-	};
+	// let postNewCommand = (newCommand) =>  {
+	// 	return $q((resolve, reject) => {
+	// 		$http.post(`${FBCreds.databaseURL}/commands.json`,
+	// 				JSON.stringify(newCommand))
+	// 			.then((ObjectFromFirebase) => {
+	// 				resolve(ObjectFromFirebase);
+	// 			})
+	// 		.catch((error) => {
+	// 			reject(error);
+	// 		});
+	// 	});
+	// };
 
-	let deleteCommand = (commandId) => {
-		console.log("delete in factory", commandId);
-		return $q((resolve, reject) => {
-			$http.delete(`${FBCreds.databaseURL}/commands/${commandId}.json`)
-				.then((ObjectFromFirebase) => {
-					resolve(ObjectFromFirebase);
-				});
-		});
-	};
+	// let deleteCommand = (commandId) => {
+	// 	console.log("delete in factory", commandId);
+	// 	return $q((resolve, reject) => {
+	// 		$http.delete(`${FBCreds.databaseURL}/commands/${commandId}.json`)
+	// 			.then((ObjectFromFirebase) => {
+	// 				resolve(ObjectFromFirebase);
+	// 			});
+	// 	});
+	// };
 
 	let getSingleCommand = (commandId) => {
 		return $q(function(resolve, reject) {
@@ -58,17 +58,19 @@ app.factory("CommandStorage", ($q, $http, FBCreds) => {
 		});
 	};
 
-	let updateCommand = (commandId, editedCommand) => {
-		return $q(function(resolve, reject) {
-			$http.patch(`${FBCreds.databaseURL}/commands/${commandId}.json`, angular.toJson(editedCommand))
-				.then(function(ObjectFromFirebase) {
-					resolve(ObjectFromFirebase);
-				})
-			.catch(function(error) {
-				reject(error);
-			});
-		});
-	};
+	// let updateCommand = (commandId, editedCommand) => {
+	// 	return $q(function(resolve, reject) {
+	// 		$http.patch(`${FBCreds.databaseURL}/commands/${commandId}.json`, angular.toJson(editedCommand))
+	// 			.then(function(ObjectFromFirebase) {
+	// 				resolve(ObjectFromFirebase);
+	// 			})
+	// 		.catch(function(error) {
+	// 			reject(error);
+	// 		});
+	// 	});
+	// };
 
-	return {getCommandList, postNewCommand, deleteCommand, getSingleCommand, updateCommand};
+	// return {getCommandList, postNewCommand, deleteCommand, getSingleCommand, updateCommand};
+	return {getCommandList, getSingleCommand};
+
 });
